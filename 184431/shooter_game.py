9077ec5356bbddacb2b5ee21e8e5808c8ef704e6
Alm0rs1k <91683934+Alm0rs1k@users.noreply.github.com>
2021-10-07 21:35:50 +0600

    Update shooter_game.py

--- 184431/shooter_game.py
+++ 184431/shooter_game.py
@@ -21,6 +21,7 @@ speed = 10
 lost = 0
 shooted = 0
 goal = 25
+lives = 3
 max_lost = 15
 finish = False
 bullets = sprite.Group()
@@ -72,13 +73,23 @@ class Bullet(GameSprite):
         if self.rect.y < 0:
             self.kill()
 
+class Asteroid(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y > 505:
+            self.rect.y = 0
+            self.rect.x = randint(1, 550)
+            self.speed = randint(1, 5)
+
 player = Player('rocket.png', 350, 430, 65, 65, 10)
 ufos = sprite.Group()
+asteroids = sprite.Group()
 for i in range(5):
     ufo = Enemy('ufo.png', randint(1, 550), 10, 50, 50, randint(1, 4))
     ufos.add(ufo)
-
-
+for i in range(5):
+    asteroid = Asteroid('asteroid.png', randint(1, 550), 10, 50, 50, randint(1, 4))
+    asteroids.add(asteroid)
 
 while game:
     for e in event.get():
@@ -97,30 +108,50 @@ while game:
         text_shooted = font1.render(
             "Сбито врагов:" + str(shooted), 1, (255, 255, 255)
         )
-
+        text_lives = font1.render(
+            "Lives:" + str(lives), 1, (150,0,0)
+        )
+        if sprite.spritecollide(player, ufos, True):
+            lives -= 1
+            ufo = Enemy('ufo.png', randint(1, 550), 10, 50, 50, randint(1, 4))
+            ufos.add(ufo)
         for c in collides:
             shooted += 1
             ufo = Enemy('ufo.png', randint(1, 550), 10, 50, 50, randint(1, 4))
             ufos.add(ufo)
-        if sprite.spritecollide(player, ufos, False) or lost >= max_lost:
+        if lives <= 0 or lost >= max_lost or sprite.spritecollide(player, asteroids, True):
             finish = True
-            window.blit(lose, (200, 200))
+            window.blit(lose, (300, 230))
             display.update()
             time.delay(3000)
             game = False
         if shooted >= goal:
             finish = True
-            window.blit(win, (200,200))
+            window.blit(win, (300,230))
             display.update()
             time.delay(3000)
             game = False
-        window.blit(text_lost, (10, 50))
-        window.blit(text_shooted, (10, 20))
+        #for i in range(1):
+        #    time.delay(7000)
+        #    for i in range(5):
+        #        text_pom = font1.render(
+        #            "POMEHI" , True, (150,150,150)
+        #        )
+        #        time.delay(randint(3000,5000))
+        #        text_pom = font1.render(
+        #            " " , True, (150,150,150)
+        #        )
+        window.blit(text_shooted, (10, 50))
+        window.blit(text_lost, (10, 20))
+        window.blit(text_lives, (570, 20))
+        #window.blit(text_pom, (randint(1, 500), randint(1, 300)))
         player.update()
         player.reset()
         ufos.update()
         ufos.draw(window)
         bullets.update()
         bullets.draw(window)
+        asteroids.update()
+        asteroids.draw(window)
         display.update()
     clock.tick(fps)
